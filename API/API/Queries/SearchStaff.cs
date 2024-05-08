70513f51f8ba00c33d4cab3cd7425737fb400387
Andrei <andrei.nicolas@hotmail.com>
2024-05-08 20:00:14 +1200

    Update SearchStaff.cs

--- API/API/Queries/SearchStaff.cs
+++ API/API/Queries/SearchStaff.cs
@@ -1,10 +1,7 @@
 public static class QueryStaff
 {
     public const string SearchStaffQuery = @"
-        SELECT TOP 100 [givenName],[idmPreferredName],[sn],[employeeNumber],[idmGUID],[idmSPID],[uid],[idmStatus],
-            [idmSSPRStatus],convert(char(11), [idmDateofBirth], 103) AS idmDateofBirth,
-            [idmGender],convert(char(11), [idmStartDate], 103) AS idmStartDate,
-            convert(char(11), [idmEndDate],103) AS idmEndDate, [idmCentreCode],[idmIsSilentUser] 
-        FROM [FIMSynchronizationService].[dbo].[mms_metaverse]
-        WHERE";
+        SELECT TOP 100 [McNuggets],[McFlurry],[BigMac],[McRib],[McChicken],[McDouble],[McFish],[McWrap],[McMuffin],[HappyMeal] 
+        FROM [McDonaldsDB].[dbo].[EmployeeTable]
+        WHERE;
 }
