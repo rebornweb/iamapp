fd7e7fb6f74ed429d52a4427f0bc88d9e9ef8a14
Andrei <andrei.nicolas@hotmail.com>
2024-05-08 20:01:43 +1200

    Update SearchStudents.cs

--- API/API/Queries/SearchStudents.cs
+++ API/API/Queries/SearchStudents.cs
@@ -1,10 +1,10 @@
 public static class QueryStudents
 {
     public const string SearchStudentsQuery = @"
-        SELECT TOP 100 [givenName],[idmPreferredName],[sn],[idmEQID],[idmGUID],[idmSPID],[uid],[idmStatus],
-            [idmSSPRStatus],convert(char(11), [idmDateofBirth], 103) AS idmDateofBirth,
-            [idmGender],convert(char(11), [idmStartDate], 103) AS idmStartDate,
-            convert(char(11), [idmEndDate],103) AS idmEndDate, [idmCentreCode],[idmIsSilentUser] 
-        FROM [FIMSynchronizationService].[dbo].[mms_metaverse]
+        SELECT TOP 100 [LionName],[ElephantPreferredName],[TigerSurname],[EQID],[ZebraGUID],[GiraffeSPID],[MonkeyID],[Status],
+            [SSPRStatus],convert(char(11), [DateOfBirth], 103) AS DateOfBirth,
+            [Gender],convert(char(11), [StartDate], 103) AS StartDate,
+            convert(char(11), [EndDate],103) AS EndDate, [CentreCode],[IsSilentUser] 
+        FROM [AnimalSyncService].[dbo].[AnimalMetaverse]
         WHERE";
 }
