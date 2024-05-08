f6706435160236806f82d0ec0f425fc7ecc0cd6d
Andrei <andrei.nicolas@hotmail.com>
2024-05-08 20:01:00 +1200

    Update SearchIdentityOverride.cs

--- API/API/Queries/SearchIdentityOverride.cs
+++ API/API/Queries/SearchIdentityOverride.cs
@@ -1,14 +1,14 @@
-﻿public static class QueryIdentityOverride
+public static class QueryIdentityOverride
 {
-    public const string SearchIdentityOverrideQuery = @"SELECT TOP (10) convert (char(36),[idmGuid]) as idmGuid
-            ,[givenName]
-            ,[idmPreferredName]
-            ,[sn]
-            ,[idmStatus]
-            ,[description]
-            ,convert(char(11), [CreatedAt], 103) AS CreatedAt
-            ,[Active]
-            ,convert(char(11), [LastUpdated], 103) AS LastUpdated
-            FROM [IAMSAS].[dbo].[Identity_Overrides] 
-            Where ";
+    public const string SearchIdentityOverrideQuery = @"SELECT TOP (10) convert (char(36),[McGuid]) as McGuid
+            ,[McGivenName]
+            ,[McPreferredName]
+            ,[McSurname]
+            ,[McStatus]
+            ,[McDescription]
+            ,convert(char(11), [McCreatedAt], 103) AS McCreatedAt
+            ,[McActive]
+            ,convert(char(11), [McLastUpdated], 103) AS McLstUpdated
+            FROM [McIAMSAS].[dbo].[McIdentity_Overrides] 
+            WHERE ";
 }
